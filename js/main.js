680eeeb4ff08eaeae2099ef78cd4b5e97385f069
Lorenz Marqueses <marqueseslorenz@gmail.com>
2020-10-16 14:14:33 +0800

    Fixed weekend interaction

--- js/main.js
+++ js/main.js
@@ -18,28 +18,33 @@ function updateDay() {
 function addSched() {
   var weekday = new Date().getDay();
   var i;
-  for (i = 1; i < sched[weekday].slots + 1; i++) {
-    document.getElementById("schedule").insertAdjacentHTML("beforeend", "<tr><td class='bodytext'><a href='https://meet.google.com/" + sched[weekday][i].link + "'>" + sched[weekday][i].name + " &#8594; </a></td><td class='bodytext'>" + sched[weekday][i].stime + "</td></tr>");
+  if (sched[weekday].slots == 0) {
+    document.getElementById("schedule").insertAdjacentHTML("beforeend", "<tr><td class='bodytext'>You have no classes today.</td></tr>")
   }
+  else {
+    for (i = 1; i < sched[weekday].slots + 1; i++) {
+      document.getElementById("schedule").insertAdjacentHTML("beforeend", "<tr><td class='bodytext'><a href='https://meet.google.com/" + sched[weekday][i].link + "'>" + sched[weekday][i].name + " &#8594; </a></td><td class='bodytext'>" + sched[weekday][i].stime + "</td></tr>");
+    };
+  };
 };
 
 function updateNext() {
   var weekday = new Date().getDay();
   var seconds = (new Date().getHours() * 3600) + (new Date().getMinutes() * 60) + new Date().getSeconds();
-  if (seconds <= sched[weekday][current+1].time) {
+  if (sched[weekday].slots == 0) {
+    document.getElementById("nextclass").innerHTML = "You have no classes today.";
+  }
+  else if (seconds <= sched[weekday][current+1].time) {
     document.getElementById("nextclass").innerHTML = "Your next class is: " + sched[weekday][current+1].name;
     document.getElementById("nextclasslink").setAttribute("href", "https://meet.google.com/" + sched[weekday][current+1].link)
     document.getElementById("nextclassbutton").setAttribute("class", "enabled");
   }
   else {
     current++;
-  };
-  if (current < sched[weekday].slots) {
-    setInterval(function () {updateNext();}, 500);
-  }
-  else {
-    current = 0;
-    document.getElementById("nextclass").innerHTML = "That was the final one! No more classes for today.";
-    document.getElementById("nextclassbutton").className = "disabled";
+    if (current >= sched[weekday].slots) {
+      current = 0;
+      document.getElementById("nextclass").innerHTML = "That was the final one! No more classes for today.";
+      document.getElementById("nextclassbutton").className = "disabled";
+    };
   };
 }
