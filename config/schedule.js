2550c28cf2b87d2d034f34c40a3136691916b961
Lorenz Marqueses <marqueseslorenz@gmail.com>
2020-10-16 11:48:10 +0800

    Web files

---
+++ config/schedule.js
@@ -0,0 +1,132 @@
+// St. Augustine
+let sched = [
+  {
+    slots: 0
+  },
+  {
+    slots: 4,
+    1: {
+      name: "Flag ceremony",
+      link: "hsq-xzgh-ijs",
+      stime: "8:00 AM",
+      time: 28800 // 8:00 AM
+    },
+    2: {
+      name: "Science",
+      link: "srb-vgiz-twd",
+      stime: "9:30 AM",
+      time: 34200 // 9:30 AM
+    },
+    3: {
+      name: "AP",
+      link: "snw-svae-kjz",
+      stime: "1:00 PM",
+      time: 46800 // 1:00 PM
+    },
+    4: {
+      name: "English",
+      link: "nun-uski-uut",
+      stime: "3:00 PM",
+      time: 54000 // 3:00 PM
+    }
+  },
+  {
+    slots: 4,
+    1: {
+      name: "Flag ceremony",
+      link: "hsq-xzgh-ijs",
+      stime: "8:00 AM",
+      time: 28800 // 8:00 AM
+    },
+    2: {
+      name: "Filipino",
+      link: "hsq-xzgh-ijs",
+      stime: "8:30 AM",
+      time: 30600 // 8:30 AM
+    },
+    3: {
+      name: "Math",
+      link: "iaw-rpjt-bwp",
+      stime: "11:30 AM",
+      time: 37800 // 11:30 AM
+    },
+    4: {
+      name: "TLE",
+      link: "fmw-mteq-bpf",
+      stime: "2:00 PM",
+      time: 50400 // 2:00 PM
+    }
+  },
+  {
+    slots: 4,
+    1: {
+      name: "Flag ceremony",
+      link: "hsq-xzgh-ijs",
+      stime: "8:00 AM",
+      time: 28800 // 8:00 AM
+    },
+    2: {
+      name: "Science",
+      link: "srb-vgiz-twd",
+      stime: "9:30 AM",
+      time: 34200 // 9:30 AM
+    },
+    3: {
+      name: "Math",
+      link: "iaw-rpjt-bwp",
+      stime: "1:00 PM",
+      time: 46800 // 1:00 PM
+    },
+    4: {
+      name: "English",
+      link: "nun-uski-uut",
+      stime: "3:00 PM",
+      time: 54000 // 3:00 PM
+    }
+  },
+  {
+    slots: 4,
+    1: {
+      name: "Flag ceremony",
+      link: "hsq-xzgh-ijs",
+      stime: "8:00 AM",
+      time: 28800 // 8:00 AM
+    },
+    2: {
+      name: "CLE",
+      link: "dur-incx-mvu",
+      stime: "8:30 AM",
+      time: 30600 // 8:30 AM
+    },
+    3: {
+      name: "MAPEH",
+      link: "pev-adrx-fzg",
+      stime: "11:30 AM",
+      time: 37800 // 11:30 AM
+    },
+    4: {
+      name: "ICT",
+      link: "bsr-jzgq-gyf",
+      stime: "2:00 PM",
+      time: 50400 // 2:00 PM
+    }
+  },
+  {
+    slots: 2,
+    1: {
+      name: "Flag ceremony",
+      link: "hsq-xzgh-ijs",
+      stime: "8:00 AM",
+      time: 28800 // 8:00 AM
+    },
+    2: {
+      name: "Homeroom",
+      link: "hsq-xzgh-ijs",
+      stime: "9:30 AM",
+      time: 34200 // 9:30 AM
+    },
+  },
+  {
+    slots: 0
+  },
+]
---
+++ js/main.js
@@ -0,0 +1,45 @@
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// Month strings
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var current = 0;
+// Weekday strings
+
+function updateDay() {
+  var today = new Date();
+  // Current date
+  var date = weekdays[today.getDay()] + ", " + months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear()
+  // Current date in day of week, MM DD, YYYY
+  var time = today.toLocaleTimeString()
+  // Time in HH:MM
+  document.getElementById("subtitle").innerHTML = "It is " + date + ", " + time;
+  setInterval(function () {updateDay(); updateNext();}, 500);
+};
+
+function addSched() {
+  var weekday = new Date().getDay();
+  var i;
+  for (i = 1; i < sched[weekday].slots + 1; i++) {
+    document.getElementById("schedule").insertAdjacentHTML("beforeend", "<tr><td class='bodytext'><a href='https://meet.google.com/" + sched[weekday][i].link + "'>" + sched[weekday][i].name + " &#8594; </a></td><td class='bodytext'>" + sched[weekday][i].stime + "</td></tr>");
+  }
+};
+
+function updateNext() {
+  var weekday = new Date().getDay();
+  var seconds = (new Date().getHours() * 3600) + (new Date().getMinutes() * 60) + new Date().getSeconds();
+  if (seconds <= sched[weekday][current+1].time) {
+    document.getElementById("nextclass").innerHTML = "Your next class is: " + sched[weekday][current+1].name;
+    document.getElementById("nextclasslink").setAttribute("href", "https://meet.google.com/" + sched[weekday][current+1].link)
+    document.getElementById("nextclassbutton").setAttribute("class", "enabled");
+  }
+  else {
+    current++;
+  };
+  if (current < sched[weekday].slots) {
+    setInterval(function () {updateNext();}, 500);
+  }
+  else {
+    current = 0;
+    document.getElementById("nextclass").innerHTML = "That was the final one! No more classes for today.";
+    document.getElementById("nextclassbutton").className = "disabled";
+  };
+}
